472100755bc4b798b40fab47ed363dfc2fea2978
kushalkolar <kushalkolar@gmail.com>
2022-04-25 23:48:11 -0400

    wrap vmin vmax, i.e. clim, for Image graphic

--- fastplotlib/graphics.py
+++ fastplotlib/graphics.py
@@ -16,11 +16,19 @@ class Image(Graphic):
     def __init__(self, data: np.ndarray, vmin: int, vmax: int, cmap: str = 'plasma', *args, **kwargs):
         super().__init__(data, *args, **kwargs)
 
-        self.world_object = pygfx.Image(
+        self.world_object: pygfx.Image = pygfx.Image(
             pygfx.Geometry(grid=pygfx.Texture(data, dim=2)),
             pygfx.ImageBasicMaterial(clim=(vmin, vmax), map=getattr(pygfx.cm, cmap))
         )
 
+    @property
+    def clim(self) -> Tuple[float, float]:
+        return self.world_object.material.clim
+
+    @clim.setter
+    def clim(self, levels: Tuple[float, float]):
+        self.world_object.material.clim = levels
+
     def update_data(self, data: np.ndarray):
-        self.world_object.geometry.data[:] = data
-        self.world_object.geometry.update_range((0, 0, 0), self.world_object.geometry.grid.size)
+        self.world_object.geometry.grid.data[:] = data
+        self.world_object.geometry.grid.update_range((0, 0, 0), self.world_object.geometry.grid.size)
