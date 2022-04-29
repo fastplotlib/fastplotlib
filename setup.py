7740400cf759a13d00ea26af99dc2ace2cdd1698
kushalkolar <kushalkolar@gmail.com>
2022-04-29 01:41:32 -0400

    setup.py includes the colormap array filesg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastplotlib',
-    version='0.1.0.a1',
+    version='0.1.0.a2',
     packages=find_packages(),
     url='https://github.com/kushalkolar/fastplotlib',
     license='Apache 2.0',
@@ -10,5 +10,6 @@ setup(
     author_email='',
     python_requires='>=3.8',
     install_requires=['numpy', 'pygfx', 'jupyterlab', 'jupyter-rfb'],
+    include_package_data=True,
     description='A fast plotting library built using the pygfx render engine'
 )
