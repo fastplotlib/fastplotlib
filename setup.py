83e318c0eea5250dffe7eefc6e2496a1e4ec2715
kushalkolar <kushalkolar@gmail.com>
2022-04-29 01:17:55 -0400

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='fastplotlib',
     version='0.1.0.a1',
-    packages=['fastplotlib'],
+    packages=find_packages(),
     url='https://github.com/kushalkolar/fastplotlib',
     license='Apache 2.0',
     author='Kushal Kolar',
     author_email='',
     python_requires='>=3.8',
-    install_requires=['numpy', 'pygfx', 'jupyterlab', 'jupyter_rfb'],
+    install_requires=['numpy', 'pygfx', 'jupyterlab', 'jupyter-rfb'],
     description='A fast plotting library built using the pygfx render engine'
 )
