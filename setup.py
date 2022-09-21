964f42f2009a24516f7999ac2364989cb8fb5259
kushalkolar <kushalkolar@gmail.com>
2022-09-21 16:30:52 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 with open(Path(__file__).parent.joinpath("README.md")) as f:
     readme = f.read()
 
-with open(Path(__file__).parent.joinpath("mesmerize_core", "VERSION"), "r") as f:
+with open(Path(__file__).parent.joinpath("fastplotlib", "VERSION"), "r") as f:
     ver = f.read().split("\n")[0]
 
 
