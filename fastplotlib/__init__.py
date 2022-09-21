f862d89f3ec31c3c58119e16e823ff7afe200011
kushalkolar <kushalkolar@gmail.com>
2022-09-21 16:27:23 -0400

    update setup

--- fastplotlib/__init__.py
+++ fastplotlib/__init__.py
@@ -2,3 +2,8 @@ from .graphics import *
 from .layouts import GridPlot
 from .subplot import Subplot
 from .plot import Plot
+from pathlib import Path
+
+
+with open(Path(__file__).parent.joinpath("VERSION"), "r") as f:
+    __version__ = f.read().split("\n")[0]
--- setup.py
+++ setup.py
@@ -1,15 +1,45 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+
+install_requires = [
+    'numpy',
+    'git+https://github.com/pygfx/pygfx.git@main',
+    'jupyterlab',
+    'jupyterlab-widgets==1.1.1',
+    'ipywidgets<8.0.0',
+    'jupyter-rfb',
+    'simplejpeg'
+]
+
+
+with open(Path(__file__).parent.joinpath("README.md")) as f:
+    readme = f.read()
+
+with open(Path(__file__).parent.joinpath("mesmerize_core", "VERSION"), "r") as f:
+    ver = f.read().split("\n")[0]
+
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Visualization",
+    "License :: OSI Approved :: Apache Software License",
+
+]
+
 
 setup(
     name='fastplotlib',
-    version='0.1.0.a4',
+    version=ver,
+    long_description=readme,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     url='https://github.com/kushalkolar/fastplotlib',
     license='Apache 2.0',
     author='Kushal Kolar',
     author_email='',
     python_requires='>=3.8',
-    install_requires=['numpy', 'pygfx', 'jupyterlab', 'jupyter-rfb', 'simplejpeg'],
+    install_requires=install_requires,
     include_package_data=True,
     description='A fast plotting library built using the pygfx render engine'
 )
