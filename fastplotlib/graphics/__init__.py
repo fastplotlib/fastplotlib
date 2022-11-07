55e6de4afecc4ee8b26301a25230ab1f5bcd8a61
Caitlin Lewis <clewis7@email.unc.edu>
2022-11-07 13:30:31 -0500

    removing heatmap stuff in order to merge this branch

--- fastplotlib/graphics/__init__.py
+++ fastplotlib/graphics/__init__.py
@@ -2,6 +2,5 @@ from .histogram import Histogram
 from .line import Line
 from .scatter import Scatter
 from .image import Image
-from .heatmap import Heatmap
 
-__all__ = ["Image", "Scatter", "Line", "Histogram", "Heatmap"]
+__all__ = ["Image", "Scatter", "Line", "Histogram"]
