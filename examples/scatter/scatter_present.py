d0ab79b388b86cd4b1199e788d6d0be19faa59a4
Caitlin Lewis <clewis7@email.unc.edu>
2023-04-20 10:05:08 -0400

    updated tests

--- examples/scatter/scatter_present.py
+++ examples/scatter/scatter_present.py
@@ -1,13 +1,14 @@
 """
-Simple Plot
+Scatter Plot
 ============
-Example showing simple plot creation and subsequent cmap change with 512 x 512 pre-saved random image.
+Example showing present feature for scatter plot.
 """
+
 # test_example = true
 
 from fastplotlib import Plot
 import numpy as np
-import imageio.v3 as iio
+from pathlib import Path
 
 from wgpu.gui.offscreen import WgpuCanvas
 from pygfx import WgpuRenderer
@@ -17,14 +18,16 @@ renderer = WgpuRenderer(canvas)
 
 plot = Plot(canvas=canvas, renderer=renderer)
 
-im = iio.imread("imageio:clock.png")
+data_path = Path(__file__).parent.parent.joinpath("data", "scatter.npy")
+data = np.load(data_path)
 
-# plot the image data
-image_graphic = plot.add_image(data=im, name="random-image")
+scatter_graphic = plot.add_scatter(data=data, sizes=3, alpha=0.7)
 
 plot.show()
 
-image_graphic.cmap = "viridis"
+plot.center_scene()
+
+scatter_graphic.present = False
 
 img = np.asarray(plot.renderer.target.draw())
 
