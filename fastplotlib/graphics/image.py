1d1be84566f8319f4c6ad511a472a4cb5eb0bf87
kushalkolar <kushalkolar@gmail.com>
2023-06-29 03:03:49 -0400

    fix heatmap tiling after linalg refactor

--- fastplotlib/graphics/image.py
+++ fastplotlib/graphics/image.py
@@ -500,8 +500,8 @@ class HeatmapGraphic(Graphic, Interaction, _ImageHeatmapSelectorsMixin):
             img.row_chunk_index = chunk[0]
             img.col_chunk_index = chunk[1]
 
-            img.position_x = x_pos
-            img.position_y = y_pos
+            img.world.position.x = x_pos
+            img.world.position.y = y_pos
 
             self.world_object.add(img)
 
