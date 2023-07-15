d34333a71210075a200623d3f3d9cfe58e50a85e
DavidVFiumano <tpt.fiumano@gmail.com>
2023-07-15 17:25:43 -0400

    Added a few tests for the new sizes feature.

---
+++ examples/desktop/scatter/scatter_size.py
@@ -0,0 +1,52 @@
+"""
+Scatter Plot
+============
+Example showing point size change for scatter plot.
+"""
+
+# test_example = true
+import numpy as np
+import fastplotlib as fpl
+
+# grid with 2 rows and 3 columns
+grid_shape = (2,1)
+
+# pan-zoom controllers for each view
+# views are synced if they have the 
+# same controller ID
+controllers = [
+    [0],
+    [1]
+]
+
+
+# you can give string names for each subplot within the gridplot
+names = [
+    ["scalar_size"],
+    ["array_size"]
+]
+
+# Create the grid plot
+plot = fpl.GridPlot(
+    shape=grid_shape,
+    controllers=controllers,
+    names=names
+)
+
+# get y_values using sin function
+y_values = 30*np.sin(np.arange(0, 20*np.pi+0.001, np.pi / 20)) # 1 thousand points
+data = np.array([[x, y] for x, y in enumerate(y_values)], dtype=np.float32)
+
+plot["scalar_size"].add_scatter(data=data, sizes=5, colors="blue") # add a set of scalar sizes
+
+non_scalar_sizes = np.abs((y_values / np.pi)) # ensure minimum size of 5
+plot["array_size"].add_scatter(data=data, sizes=non_scalar_sizes, colors="red")
+
+for graph in plot:
+    graph.auto_scale(maintain_aspect=True)
+
+plot.show()
+
+if __name__ == "__main__":
+    print(__doc__)
+    fpl.run()
