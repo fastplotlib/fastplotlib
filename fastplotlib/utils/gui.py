f21f7aab58a55882937a45644526328fe6a5e7a1
Almar Klein <almar@almarklein.org>
2024-03-29 12:27:57 +0100

    fixes

--- fastplotlib/utils/gui.py
+++ fastplotlib/utils/gui.py
@@ -4,12 +4,13 @@ from pathlib import Path
 
 import wgpu
 
-from . import _notebook_print_banner
+
+# --- Prepare
 
 
-# Triage. Ultimately, we let wgpu-py decide, but we can prime things a bit to
-# create our own preferred order, by importing a Qt lib. But we only do this
-# if no GUI has been imported yet.
+# Ultimately, we let wgpu-py decide, but we can prime things a bit to create our
+# own preferred order, by importing a Qt lib. But we only do this if no GUI has
+# been imported yet.
 
 # Qt libs that we will try to import
 qt_libs = ["PySide6", "PyQt6", "PySide2", "PyQt5"]
@@ -27,30 +28,19 @@ if not already_imported:
         else:
             break
 
+
+# --- Triage
+
+
 # Let wgpu do the auto gui selection
 from wgpu.gui.auto import WgpuCanvas, run
 
 # Get the name of the backend ('qt', 'glfw', 'jupyter')
 GUI_BACKEND = WgpuCanvas.__module__.split(".")[-1]
+IS_JUPYTER = GUI_BACKEND == "jupyter"
 
-IS_JUPYTER = False
 
-if GUI_BACKEND == "jupyter":
-    IS_JUPYTER = True
-    _notebook_print_banner()
-
-elif GUI_BACKEND == "qt":
-    from wgpu.gui.qt import get_app, libname
-
-    # create and store ref to qt app
-    _qt_app = get_app()
-
-    # Import submodules of PySide6/PyQt6/PySid2/PyQt5
-    # For the way that fpl uses Qt, the supported Qt libs seems compatible enough.
-    # If necessary we can do some qtpy-like monkey-patching here.
-    QtCore = importlib.import_module(".QtCore", libname)
-    QtGui = importlib.import_module(".QtGui", libname)
-    QtWidgets = importlib.import_module(".QtWidgets", libname)
+# --- Some backend-specific preparations
 
 
 def _notebook_print_banner():
@@ -70,7 +60,7 @@ def _notebook_print_banner():
     display(image)
 
     # print logo and adapter info
-    adapters = [a for a in wgpu.gui.enumerate_adapters()]
+    adapters = [a for a in wgpu.gpu.enumerate_adapters()]
     adapters_info = [a.request_adapter_info() for a in adapters]
 
     if len(adapters) > 0:
@@ -96,3 +86,20 @@ def _notebook_print_banner():
 
         output_str = f"{charactor}{default}| {device} | {atype} | {backend} | {driver}"
         print(output_str)
+
+
+if GUI_BACKEND == "jupyter":
+    _notebook_print_banner()
+
+elif GUI_BACKEND == "qt":
+    from wgpu.gui.qt import get_app, libname
+
+    # create and store ref to qt app
+    _qt_app = get_app()
+
+    # Import submodules of PySide6/PyQt6/PySid2/PyQt5
+    # For the way that fpl uses Qt, the supported Qt libs seems compatible enough.
+    # If necessary we can do some qtpy-like monkey-patching here.
+    QtCore = importlib.import_module(".QtCore", libname)
+    QtGui = importlib.import_module(".QtGui", libname)
+    QtWidgets = importlib.import_module(".QtWidgets", libname)
