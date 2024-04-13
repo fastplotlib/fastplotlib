940b9b0d257301734c600078bab123da3421786c
kushalkolar <kushalkolar@gmail.com>
2024-04-12 21:32:02 -0400

    update qt examples

--- examples/qt/imagewidget.py
+++ examples/qt/imagewidget.py
@@ -4,14 +4,13 @@ Use ImageWidget to display one or multiple image sequences
 import numpy as np
 from PyQt6 import QtWidgets
 import fastplotlib as fpl
+import imageio.v3 as iio
 
-# Qt app MUST be instantiated before creating any fpl objects, or any other Qt objects
-app = QtWidgets.QApplication([])
 
 images = np.random.rand(100, 512, 512)
 
-# create image widget, force Qt canvas so it doesn't pick glfw
-iw = fpl.ImageWidget(images, grid_plot_kwargs={"canvas": "qt"})
+# fastplotlib and wgpu will auto-detect if Qt is imported and then use the Qt canvas
+iw = fpl.ImageWidget(images)
 iw.show()
 iw.widget.resize(800, 800)
 
@@ -20,10 +19,14 @@ images_list = [np.random.rand(100, 512, 512) for i in range(9)]
 
 iw_mult = fpl.ImageWidget(
     images_list,
-    grid_plot_kwargs={"canvas": "qt"},
     cmap="viridis"
 )
 iw_mult.show()
 iw_mult.widget.resize(800, 800)
 
-app.exec()
+# image widget with rgb data
+rgb_video = iio.imread("imageio:cockatoo.mp4")
+iw_rgb = fpl.ImageWidget(rgb_video, rgb=[True])
+iw_rgb.show()
+
+fpl.run()
