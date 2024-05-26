f295356676ad86a4120c9372dda0263baf19bbbe
kushalkolar <kushalkolar@gmail.com>
2024-05-26 00:17:08 -0400

    cleanup

--- fastplotlib/graphics/selectors/__init__.py
+++ fastplotlib/graphics/selectors/__init__.py
@@ -2,11 +2,8 @@ from ._linear import LinearSelector
 from ._linear_region import LinearRegionSelector
 from ._polygon import PolygonSelector
 
-from ._sync import Synchronizer
-
 __all__ = [
     "LinearSelector",
     "LinearRegionSelector",
     "PolygonSelector",
-    "Synchronizer",
 ]
