9ca2c2a4c413b60eb98e63f1ba873ebdde453644
kushalkolar <kushalkolar@gmail.com>
2024-06-06 21:03:16 -0400

    test remove event hanlders common fea

--- tests/test_common_features.py
+++ tests/test_common_features.py
@@ -31,6 +31,7 @@ graphic_kinds = [
 
 
 RETURN_EVENT_VALUE: FeatureEvent = None
+DECORATED_EVENT_VALUE: FeatureEvent = None
 
 
 def return_event(ev: FeatureEvent):
@@ -54,6 +55,33 @@ def test_name(graphic):
     assert RETURN_EVENT_VALUE.graphic is graphic
     assert RETURN_EVENT_VALUE.target is graphic.world_object
     assert RETURN_EVENT_VALUE.info["value"] == "new_name"
+
+    # check removing event handler
+    RETURN_EVENT_VALUE = None
+    graphic.remove_event_handler(return_event, "name")
+    assert len(graphic._event_handlers["name"]) == 0
+
+    graphic.name = "new_name2"
+
+    assert RETURN_EVENT_VALUE is None
+    assert graphic.name == "new_name2"
+
+    # check adding event with decorator
+    global DECORATED_EVENT_VALUE
+    DECORATED_EVENT_VALUE = None
+
+    @graphic.add_event_handler("name")
+    def decorated_handler(ev):
+        global DECORATED_EVENT_VALUE
+        DECORATED_EVENT_VALUE = ev
+
+    graphic.name = "test_dec"
+    assert graphic.name == "test_dec"
+
+    assert DECORATED_EVENT_VALUE.type == "name"
+    assert DECORATED_EVENT_VALUE.graphic is graphic
+    assert DECORATED_EVENT_VALUE.target is graphic.world_object
+    assert DECORATED_EVENT_VALUE.info["value"] == "test_dec"
 
 
 @pytest.mark.parametrize("graphic", [make_graphic(k, name="init_name") for k in graphic_kinds])
@@ -83,6 +111,33 @@ def test_offset(graphic):
     assert RETURN_EVENT_VALUE.graphic is graphic
     assert RETURN_EVENT_VALUE.target is graphic.world_object
     npt.assert_almost_equal(RETURN_EVENT_VALUE.info["value"], (1., 2., 3.))
+
+    # check removing event handler
+    RETURN_EVENT_VALUE = None
+    graphic.remove_event_handler(return_event, "offset")
+    assert len(graphic._event_handlers["offset"]) == 0
+
+    graphic.offset = (4, 5, 6)
+
+    assert RETURN_EVENT_VALUE is None
+    npt.assert_almost_equal(graphic.offset, (4., 5., 6.))
+
+    # check adding event with decorator
+    global DECORATED_EVENT_VALUE
+    DECORATED_EVENT_VALUE = None
+
+    @graphic.add_event_handler("offset")
+    def decorated_handler(ev):
+        global DECORATED_EVENT_VALUE
+        DECORATED_EVENT_VALUE = ev
+
+    graphic.offset = (7, 8, 9)
+    npt.assert_almost_equal(graphic.offset, (7., 8., 9.))
+
+    assert DECORATED_EVENT_VALUE.type == "offset"
+    assert DECORATED_EVENT_VALUE.graphic is graphic
+    assert DECORATED_EVENT_VALUE.target is graphic.world_object
+    assert DECORATED_EVENT_VALUE.info["value"] == (7., 8., 9.)
 
 
 @pytest.mark.parametrize("graphic", [make_graphic(k, offset=(3., 4., 5.)) for k in graphic_kinds])
@@ -115,6 +170,33 @@ def test_rotation(graphic):
     assert RETURN_EVENT_VALUE.target is graphic.world_object
     npt.assert_almost_equal(RETURN_EVENT_VALUE.info["value"], (0., 0., 0.30001427, 0.95393471))
 
+    # check removing event handler
+    RETURN_EVENT_VALUE = None
+    graphic.remove_event_handler(return_event, "rotation")
+    assert len(graphic._event_handlers["rotation"]) == 0
+
+    graphic.rotation = (0, 0, 0, 1)
+
+    assert RETURN_EVENT_VALUE is None
+    npt.assert_almost_equal(graphic.rotation, (0, 0, 0, 1))
+
+    # check adding event with decorator
+    global DECORATED_EVENT_VALUE
+    DECORATED_EVENT_VALUE = None
+
+    @graphic.add_event_handler("rotation")
+    def decorated_handler(ev):
+        global DECORATED_EVENT_VALUE
+        DECORATED_EVENT_VALUE = ev
+
+    graphic.rotation = (0, 0, 0.6, 0.8)
+    npt.assert_almost_equal(graphic.rotation, (0, 0, 0.6, 0.8))
+
+    assert DECORATED_EVENT_VALUE.type == "rotation"
+    assert DECORATED_EVENT_VALUE.graphic is graphic
+    assert DECORATED_EVENT_VALUE.target is graphic.world_object
+    assert DECORATED_EVENT_VALUE.info["value"] == (0, 0, 0.6, 0.8)
+
 
 @pytest.mark.parametrize("graphic", [make_graphic(k, rotation=(0., 0., 0.30001427, 0.95393471)) for k in graphic_kinds])
 def test_rotation(graphic):
@@ -138,10 +220,39 @@ def test_visible(graphic):
     assert graphic.visible is False
     assert graphic.world_object.visible is False
 
+    global RETURN_EVENT_VALUE
+
     assert RETURN_EVENT_VALUE.type == "visible"
     assert RETURN_EVENT_VALUE.graphic is graphic
     assert RETURN_EVENT_VALUE.target is graphic.world_object
-    assert RETURN_EVENT_VALUE.info["value"] == False
+    assert RETURN_EVENT_VALUE.info["value"] is False
+
+    # check removing event handler
+    RETURN_EVENT_VALUE = None
+    graphic.remove_event_handler(return_event, "visible")
+    assert len(graphic._event_handlers["visible"]) == 0
+
+    graphic.visible = True
+
+    assert RETURN_EVENT_VALUE is None
+    assert graphic.visible is True
+
+    # check adding event with decorator
+    global DECORATED_EVENT_VALUE
+    DECORATED_EVENT_VALUE = None
+
+    @graphic.add_event_handler("visible")
+    def decorated_handler(ev):
+        global DECORATED_EVENT_VALUE
+        DECORATED_EVENT_VALUE = ev
+
+    graphic.visible = False
+    assert graphic.visible is False
+
+    assert DECORATED_EVENT_VALUE.type == "visible"
+    assert DECORATED_EVENT_VALUE.graphic is graphic
+    assert DECORATED_EVENT_VALUE.target is graphic.world_object
+    assert DECORATED_EVENT_VALUE.info["value"] is False
 
 
 @pytest.mark.parametrize("graphic", [make_graphic(k, visible=False) for k in graphic_kinds])
