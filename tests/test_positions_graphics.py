105cd86da5ec2a977f615cd757104730a0b88372
kushalkolar <kushalkolar@gmail.com>
2024-06-06 21:12:12 -0400

    start test positions graphics

---
+++ tests/test_positions_graphics.py
@@ -0,0 +1,46 @@
+import numpy as np
+from numpy import testing as npt
+
+import pygfx
+
+import fastplotlib as fpl
+from fastplotlib.graphics._features import VertexPositions, VertexColors, VertexCmap, UniformColor, UniformSizes, PointsSizesFeature
+
+
+# TODO: use same functions to generate data and colors for graphics as the buffer test modules
+
+
+def test_data_slice():
+    pass
+
+
+def test_color_slice():
+    pass
+
+
+def test_cmap_slice():
+    pass
+
+
+def test_sizes_slice():
+    pass
+
+
+def test_change_thickness():
+    pass
+
+
+def test_uniform_color():
+    pass
+
+
+def test_uniform_size():
+    pass
+
+
+def test_create_line():
+    pass
+
+
+def test_create_scatter():
+    pass
