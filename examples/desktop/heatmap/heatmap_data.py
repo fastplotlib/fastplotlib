3079fa633644456bce53bed004c715e94f3da584
kushalkolar <kushalkolar@gmail.com>
2024-06-11 04:43:39 -0400

    smaller hm data test for CI

--- examples/desktop/heatmap/heatmap_data.py
+++ examples/desktop/heatmap/heatmap_data.py
@@ -12,11 +12,11 @@ import numpy as np
 
 fig = fpl.Figure()
 
-xs = np.linspace(0, 1_000, 10_000)
+xs = np.linspace(0, 1_000, 9_000)
 
 sine = np.sin(np.sqrt(xs))
 
-data = np.vstack([sine * i for i in range(20_000)])
+data = np.vstack([sine * i for i in range(9_000)])
 
 # plot the image data
 img = fig[0, 0].add_image(data=data, name="heatmap")
@@ -26,10 +26,10 @@ fig.show()
 fig.canvas.set_logical_size(1500, 1500)
 
 fig[0, 0].auto_scale()
-cosine = np.cos(np.sqrt(xs))
+cosine = np.cos(np.sqrt(xs)[:3000])
 
-# change first 10,000 rows and 9,000 columns
-img.data[:10_000, :9000] = np.vstack([cosine[:9000] * i * 2 for i in range(10_000)])
+# change first 2,000 rows and 3,000 columns
+img.data[:2_000, :3_000] = np.vstack([cosine * i * 4 for i in range(2_000)])
 
 if __name__ == "__main__":
     print(__doc__)
