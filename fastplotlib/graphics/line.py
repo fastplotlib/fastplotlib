43a9f8d7af912f7ef34b3075d263467b69a8b9f5
kushalkolar <kushalkolar@gmail.com>
2024-06-11 01:38:14 -0400

    line linear selector init logic

--- fastplotlib/graphics/line.py
+++ fastplotlib/graphics/line.py
@@ -114,11 +114,16 @@ class LineGraphic(PositionsGraphic):
 
         Parameters
         ----------
-        selection: float
-            initial position of the selector
+        Parameters
+        ----------
+        selection: float, optional
+            selected point on the linear selector, computed from data if not provided
 
-        padding: float
-            pad the length of the selector
+        axis: str, default "x"
+            axis that the selector resides on
+
+        padding: float, default 0.0
+            Extra padding to extend the linear selector along the orthogonal axis to make it easier to interact with.
 
         kwargs
             passed to :class:`.LinearSelector`
@@ -129,32 +134,10 @@ class LineGraphic(PositionsGraphic):
 
         """
 
-        data = self.data.value[~np.any(np.isnan(self.data.value), axis=1)]
-
-        if axis == "x":
-            # xvals
-            axis_vals = data[:, 0]
-
-            # yvals to get size and center
-            magn_vals = data[:, 1]
-        elif axis == "y":
-            axis_vals = data[:, 1]
-            magn_vals = data[:, 0]
+        bounds_init, limits, size, center = self._get_linear_selector_init_args(axis, padding)
 
         if selection is None:
-            selection = axis_vals[0]
-        limits = axis_vals[0], axis_vals[-1]
-
-        if not limits[0] <= selection <= limits[1]:
-            raise ValueError(
-                f"the passed selection: {selection} is beyond the limits: {limits}"
-            )
-
-        # width or height of selector
-        size = int(np.ptp(magn_vals) * 1.5 + padding)
-
-        # center of selector along the other axis
-        center = np.nanmean(magn_vals)
+            selection = bounds_init[0]
 
         selector = LinearSelector(
             selection=selection,
@@ -174,7 +157,11 @@ class LineGraphic(PositionsGraphic):
         return weakref.proxy(selector)
 
     def add_linear_region_selector(
-        self, padding: float = 0.0, axis: str = "x", **kwargs
+        self,
+        selection: tuple[float, float] = None,
+        padding: float = 0.0,
+        axis: str = "x",
+        **kwargs
     ) -> LinearRegionSelector:
         """
         Add a :class:`.LinearRegionSelector`. Selectors are just ``Graphic`` objects, so you can manage,
@@ -182,8 +169,14 @@ class LineGraphic(PositionsGraphic):
 
         Parameters
         ----------
-        padding: float, default 100.0
-            Extends the linear selector along the y-axis to make it easier to interact with.
+        selection: (float, float), optional
+            the starting bounds of the linear region selector, computed from data if not provided
+
+        axis: str, default "x"
+            axis that the selector resides on
+
+        padding: float, default 0.0
+            Extra padding to extend the linear region selector along the orthogonal axis to make it easier to interact with.
 
         kwargs
             passed to ``LinearRegionSelector``
@@ -195,6 +188,34 @@ class LineGraphic(PositionsGraphic):
 
         """
 
+        bounds_init, limits, size, center = self._get_linear_selector_init_args(axis, padding)
+
+        if selection is None:
+            selection = bounds_init
+
+        # create selector
+        selector = LinearRegionSelector(
+            selection=selection,
+            limits=limits,
+            size=size,
+            center=center,
+            axis=axis,
+            parent=weakref.proxy(self),
+            **kwargs,
+        )
+
+        self._plot_area.add_graphic(selector, center=False)
+
+        # place selector below this graphic
+        selector.offset = selector.offset + (0.0, 0.0, self.offset[-1] - 1)
+
+        # PlotArea manages this for garbage collection etc. just like all other Graphics
+        # so we should only work with a proxy on the user-end
+        return weakref.proxy(selector)
+
+    # TODO: this method is a bit of a mess, can refactor later
+    def _get_linear_selector_init_args(self, axis: str, padding) -> tuple[tuple[float, float], tuple[float, float], float, float]:
+        # computes args to create selectors
         n_datapoints = self.data.value.shape[0]
         value_25p = int(n_datapoints / 4)
 
@@ -220,65 +241,4 @@ class LineGraphic(PositionsGraphic):
         # center of selector along the other axis
         center = np.nanmean(magn_vals)
 
-        # create selector
-        selector = LinearRegionSelector(
-            selection=bounds_init,
-            limits=limits,
-            size=size,
-            center=center,
-            axis=axis,
-            parent=weakref.proxy(self),
-            **kwargs,
-        )
-
-        self._plot_area.add_graphic(selector, center=False)
-
-        # place selector below this graphic
-        selector.offset = selector.offset + (0.0, 0.0, self.offset[-1] - 1)
-
-        # PlotArea manages this for garbage collection etc. just like all other Graphics
-        # so we should only work with a proxy on the user-end
-        return weakref.proxy(selector)
-
-    # TODO: this method is a bit of a mess, can refactor later
-    def _get_linear_selector_init_args(self, padding: float, **kwargs):
-        # computes initial bounds, limits, size and origin of linear selectors
-        data = self.data.value
-
-        if "axis" in kwargs.keys():
-            axis = kwargs["axis"]
-        else:
-            axis = "x"
-
-        if axis == "x":
-            offset = self.offset[0]
-            # x limits
-            limits = (data[0, 0] + offset, data[-1, 0] + offset)
-
-            # height + padding
-            size = np.ptp(data[:, 1]) + padding
-
-            # initial position of the selector
-            position_y = (data[:, 1].min() + data[:, 1].max()) / 2
-
-            # need y offset too for this
-            origin = (limits[0] - offset, position_y + self.offset[1])
-
-        else:
-            offset = self.offset[1]
-            # y limits
-            limits = (data[0, 1] + offset, data[-1, 1] + offset)
-
-            # width + padding
-            size = np.ptp(data[:, 0]) + padding
-
-            # initial position of the selector
-            position_x = (data[:, 0].min() + data[:, 0].max()) / 2
-
-            # need x offset too for this
-            origin = (position_x + self.offset[0], limits[0] - offset)
-
-        # initial bounds are 20% of the limits range
-        bounds_init = (limits[0], int(np.ptp(limits) * 0.2) + offset)
-
-        return bounds_init, limits, size, origin, axis
+        return bounds_init, limits, size, center
