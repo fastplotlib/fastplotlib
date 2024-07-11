7c5e294c6030fa6957c4a0f13e3e3dac8a9ffcb5
kushalkolar <kushalkolar@gmail.com>
2024-07-11 00:01:00 -0400

    very basic stuff works

--- fastplotlib/layouts/_subplot.py
+++ fastplotlib/layouts/_subplot.py
@@ -10,6 +10,7 @@ from ..graphics import TextGraphic
 from ._utils import make_canvas_and_renderer, create_camera, create_controller
 from ._plot_area import PlotArea
 from ._graphic_methods_mixin import GraphicMethodsMixin
+from ._axes import Axes
 
 
 class Subplot(PlotArea, GraphicMethodsMixin):
@@ -88,12 +89,6 @@ class Subplot(PlotArea, GraphicMethodsMixin):
 
         self.spacing = 2
 
-        self._axes: pygfx.AxesHelper = pygfx.AxesHelper(size=100)
-        for arrow in self._axes.children:
-            self._axes.remove(arrow)
-
-        self._grid: pygfx.GridHelper = pygfx.GridHelper(size=100, thickness=1)
-
         self._title_graphic: TextGraphic = None
 
         super(Subplot, self).__init__(
@@ -115,6 +110,10 @@ class Subplot(PlotArea, GraphicMethodsMixin):
 
         if self.name is not None:
             self.set_title(self.name)
+
+        self.axes = Axes(self)
+        self.scene.add(self.axes.world_object)
+        self.add_animations(self.axes.animate)
 
     @property
     def name(self) -> str:
@@ -186,20 +185,6 @@ class Subplot(PlotArea, GraphicMethodsMixin):
             rect = rect + dv.get_parent_rect_adjust()
 
         return rect
-
-    def set_axes_visibility(self, visible: bool):
-        """Toggles axes visibility."""
-        if visible:
-            self.scene.add(self._axes)
-        else:
-            self.scene.remove(self._axes)
-
-    def set_grid_visibility(self, visible: bool):
-        """Toggles grid visibility."""
-        if visible:
-            self.scene.add(self._grid)
-        else:
-            self.scene.remove(self._grid)
 
 
 class Dock(PlotArea):
