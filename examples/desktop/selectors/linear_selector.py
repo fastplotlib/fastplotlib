617e234ab6040ab408a1c48448b3ef6c9e8ec360
kushalkolar <kushalkolar@gmail.com>
2024-07-24 13:43:50 -0400

    comments

--- examples/desktop/selectors/linear_selector.py
+++ examples/desktop/selectors/linear_selector.py
@@ -11,26 +11,33 @@ Example showing how to use a `LinearSelector` with lines, line collections, and 
 import fastplotlib as fpl
 import numpy as np
 
+# create some data
 xs = np.linspace(0, 10 * np.pi, 100)
 sine = np.column_stack([xs, np.sin(xs)])
 cosine = np.column_stack([xs, np.cos(xs)])
 
+# a varying sine-cosine quilted pattern
 image_xs, image_ys = np.meshgrid(xs, xs)
 multiplier = np.linspace(0, 10, 100)
 image_data = multiplier * np.sin(image_xs) + multiplier * np.cos(image_ys)
 
+# create a figure
 figure = fpl.Figure(
     shape=(2, 2),
     size=(700, 560)
 )
 
+# line of a single sine wave from 0 - 10π
 line = figure[0, 0].add_line(sine, cmap="jet")
+
+# add a linear selector to the line
 line_selector = line.add_linear_selector()
 
 line_selector_text = (f"x value: {line_selector.selection / np.pi:.2f}π\n"
                       f"y value: {line.data[0, 1]:.2f}\n"
                       f"index: {line_selector.get_selected_index()}")
 
+# a label that will change to display line data based on the linear selector
 line_selection_label = figure[0, 0].add_text(
     line_selector_text,
     offset=(0., 1.75, 0.),
@@ -42,19 +49,23 @@ line_selection_label = figure[0, 0].add_text(
 )
 
 
+# add an event handler using a decorator, selectors are just like other graphics
 @line_selector.add_event_handler("selection")
 def line_selector_changed(ev):
     selection = ev.info["value"]
     index = ev.get_selected_index()
 
+    # set text to display selection data
     line_selection_label.text = \
         (f"x value: {selection / np.pi:.2f}π\n"
          f"y value: {line.data[index, 1]:.2f}\n"
          f"index: {index}")
 
+    # set text color based on line color at selection index
     line_selection_label.face_color = line.colors[index]
 
 
+# line stack, sine and cosine wave
 line_stack = figure[0, 1].add_line_stack([sine, cosine], colors=["magenta", "cyan"], separation=1)
 line_stack_selector = line_stack.add_linear_selector()
 
@@ -63,6 +74,7 @@ line_stack_selector_text = (f"x value: {line_stack_selector.selection / np.pi:.2
                             f"sine y value: {line_stack[0].data[0, 1]:.2f}\n"
                             f"cosine y value: {line_stack[1].data[0, 1]:.2f}\n")
 
+# a label that will change to display line_stack data based on the linear selector
 line_stack_selector_label = figure[0, 1].add_text(
     line_stack_selector_text,
     offset=(0., 7.0, 0.),
@@ -72,14 +84,16 @@ line_stack_selector_label = figure[0, 1].add_text(
 )
 
 
+# add an event handler using a decorator
 @line_stack_selector.add_event_handler("selection")
 def line_stack_selector_changed(ev):
     selection = ev.info["value"]
 
-    # linear selectors on line collections return a
-    # list of indices for each graphic in the collection
+    # a linear selectors one a line collection returns a
+    # list of selected indices for each graphic in the collection
     index = ev.get_selected_index()[0]
 
+    # set text to display selection data
     line_stack_selector_label.text = \
         (f"x value: {selection / np.pi:.2f}π\n"
          f"index: {index}\n"
@@ -87,16 +101,25 @@ def line_stack_selector_changed(ev):
          f"cosine y value: {line_stack[1].data[index, 1]:.2f}\n")
 
 
+# create an image
 image = figure[1, 0].add_image(image_data)
+
+# add a row selector
 image_row_selector = image.add_linear_selector(axis="y")
+
+# add column selector
 image_col_selector = image.add_linear_selector()
 
+# make a line to indicate row data
 line_image_row = figure[1, 1].add_line(image.data[0])
 
+# make a line to indicate column data
 line_image_col_data = np.column_stack([image.data[:, 0], np.arange(100)])
 line_image_col = figure[1, 1].add_line(line_image_col_data)
 
 
+# callbacks to change the line data in subplot [1, 1]
+# to display selected row and selected column data
 def image_row_selector_changed(ev):
     ix = ev.get_selected_index()
     new_data = image.data[ix]
@@ -111,11 +134,13 @@ def image_col_selector_changed(ev):
     line_image_col.data[:, 0] = new_data
 
 
+# add event handlers, you can also use a decorator
 image_row_selector.add_event_handler(image_row_selector_changed, "selection")
 image_col_selector.add_event_handler(image_col_selector_changed, "selection")
 
 figure.show(maintain_aspect=False)
 
+# some axes and camera zoom settings
 for subplot in [figure[0, 0], figure[0, 1]]:
     subplot.axes.auto_grid = False
     subplot.axes.grids.xy.major_step = (np.pi, 1)
