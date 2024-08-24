c12b2c8db02d645fdf247f0ac5de14dd0ace0f2b
Caitlin Lewis <69729525+clewis7@users.noreply.github.com>
2024-08-24 10:21:12 -0400

    add rectangular region selector (#576)
    * start working on adding rectangle region selector
    
    * rename selector tool
    
    * update line setting, remove origin
    
    * fix selection feature
    
    * fix pick write issue
    
    * rename file, fix line initialization
    
    * add fixed axis if given, make sure bounds enforced
    
    * add methods for graphics
    
    * get selected ixs and data for images
    
    * add rectangle methods
    
    * better lower bound limit for collections
    
    * add vertices
    
    * screen space instead of world space for vertices
    
    * change vertices to square, change vertex color
    
    * fix selection for line collections
    
    * add example
    
    * update api docs, add example
    
    * lint
    
    * update conf and api files
    
    * this file should be deleted
    
    * requested changes, update with better example, more comments
    
    * fix offset when adding rectangle selector so fill events happen
    
    * update api docs
    
    * requested changes

--- fastplotlib/graphics/selectors/__init__.py
+++ fastplotlib/graphics/selectors/__init__.py
@@ -1,6 +1,7 @@
 from ._linear import LinearSelector
 from ._linear_region import LinearRegionSelector
 from ._polygon import PolygonSelector
+from ._rectangle import RectangleSelector
 
 
-__all__ = ["LinearSelector", "LinearRegionSelector"]
+__all__ = ["LinearSelector", "LinearRegionSelector", "RectangleSelector"]
