628bad5e0896f1ed059fd917ec8e603efdd76178
kushalkolar <kushalkolar@gmail.com>
2024-09-19 00:58:40 -0400

    add iw dir to tests

--- examples/tests/testutils.py
+++ examples/tests/testutils.py
@@ -18,6 +18,7 @@ diffs_dir = examples_dir / "diffs"
 # examples live in themed sub-folders
 example_globs = [
     "image/*.py",
+    "image_widget/*.py",
     "heatmap/*.py",
     "scatter/*.py",
     "line/*.py",
