f2418e2f22ed2c5e6e0f8b330b338f04eeb67f91
kushalkolar <kushalkolar@gmail.com>
2024-09-19 02:25:27 -0400

    smaller heatmap

--- examples/desktop/heatmap/heatmap.py
+++ examples/desktop/heatmap/heatmap.py
@@ -12,18 +12,17 @@ import numpy as np
 
 figure = fpl.Figure(size=(700, 560))
 
-xs = np.linspace(0, 1_000, 9_000, dtype=np.float32)
+xs = np.linspace(0, 1_000, 9_000, dtype=np.float16)
 
 sine = np.sin(np.sqrt(xs))
 
-data = np.vstack([sine * i for i in range(15_000)])
+data = np.vstack([sine * i for i in range(10_000)])
 
 # plot the image data
 img = figure[0, 0].add_image(data=data, name="heatmap")
 del data
 
 figure.show()
-
 
 # NOTE: `if __name__ == "__main__"` is NOT how to use fastplotlib interactively
 # please see our docs for using fastplotlib interactively in ipython and jupyter
