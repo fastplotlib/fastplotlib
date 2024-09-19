b89a194d00c7429dfa52da0222b503910f327739
kushalkolar <kushalkolar@gmail.com>
2024-09-19 01:29:51 -0400

    fix iw movie example

--- examples/desktop/image_widget/image_widget_single_video.py
+++ examples/desktop/image_widget/image_widget_single_video.py
@@ -15,23 +15,25 @@ import numpy as np
 
 movie = iio.imread("imageio:cockatoo.mp4")
 
-iw_movie = fpl.ImageWidget(data=movie, rgb=[True])
+iw = fpl.ImageWidget(movie, rgb=[True], figure_kwargs={"size": (700, 560)})
 
 # ImageWidget supports setting window functions the `time` "t" or `volume` "z" dimension
 # These can also be given as kwargs to `ImageWidget` during instantiation
 # to set a window function, give a dict in the form of {dim: (func, window_size)}
-iw_movie.window_funcs = {"t": (np.mean, 13)}
+iw.window_funcs = {"t": (np.mean, 13)}
 
-# change the winow size
-iw_movie.window_funcs["t"].window_size = 33
+# change the window size
+iw.window_funcs["t"].window_size = 33
 
 # change the function
-iw_movie.window_funcs["t"].func = np.max
+iw.window_funcs["t"].func = np.max
 
 # or reset it
-iw_movie.window_funcs = None
+iw.window_funcs = None
 
-figure = iw_movie.figure
+iw.show()
+
+figure = iw.figure
 
 # NOTE: `if __name__ == "__main__"` is NOT how to use fastplotlib interactively
 # please see our docs for using fastplotlib interactively in ipython and jupyter
