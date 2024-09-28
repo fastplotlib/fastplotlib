bb3524f97bf80bbbcccaa7bdf93574be527ab925
kushalkolar <kushalkolar@gmail.com>
2024-09-28 02:56:28 -0400

    idk wtf is going on, makes no senze, just going to show code and not render image linear selector example for no

--- examples/desktop/selectors/linear_selector_image.py
+++ examples/desktop/selectors/linear_selector_image.py
@@ -7,7 +7,7 @@ displays the data for the selector row and column.
 """
 
 # test_example = false
-# sphinx_gallery_pygfx_docs = 'screenshot'
+# sphinx_gallery_pygfx_docs = 'code'
 
 import fastplotlib as fpl
 from imageio import v3 as iio
@@ -20,44 +20,44 @@ figure = fpl.Figure((1, 3), size=(700, 300), names=[["image", "selected row data
 # create an image
 image = figure[0, 0].add_image(image_data)
 
-# # add a row selector
-# image_row_selector = image.add_linear_selector(axis="y")
-#
-# # add column selector
-# image_col_selector = image.add_linear_selector()
+# add a row selector
+image_row_selector = image.add_linear_selector(axis="y")
 
-# # make a line to indicate row data
-# line_image_row = figure[0, 1].add_line(image.data[0])
-#
-# # make a line to indicate column data
-# line_image_col = figure[0, 2].add_line(image.data[:, 0])
-#
-#
-# # callbacks to change the line data in subplot [0, 1]
-# # to display selected row and selected column data
-# def image_row_selector_changed(ev):
-#     ix = ev.get_selected_index()
-#     new_data = image.data[ix]
-#     # set y values of line with the row data
-#     line_image_row.data[:, 1] = new_data
-#
-#
-# def image_col_selector_changed(ev):
-#     ix = ev.get_selected_index()
-#     new_data = image.data[:, ix]
-#     # set y values of line with the column data
-#     line_image_col.data[:, 1] = new_data
-#
-#
-# # add event handlers, you can also use a decorator
-# image_row_selector.add_event_handler(image_row_selector_changed, "selection")
-# image_col_selector.add_event_handler(image_col_selector_changed, "selection")
+# add column selector
+image_col_selector = image.add_linear_selector()
+
+# make a line to indicate row data
+line_image_row = figure[0, 1].add_line(image.data[0])
+
+# make a line to indicate column data
+line_image_col = figure[0, 2].add_line(image.data[:, 0])
+
+
+# callbacks to change the line data in subplot [0, 1]
+# to display selected row and selected column data
+def image_row_selector_changed(ev):
+    ix = ev.get_selected_index()
+    new_data = image.data[ix]
+    # set y values of line with the row data
+    line_image_row.data[:, 1] = new_data
+
+
+def image_col_selector_changed(ev):
+    ix = ev.get_selected_index()
+    new_data = image.data[:, ix]
+    # set y values of line with the column data
+    line_image_col.data[:, 1] = new_data
+
+
+# add event handlers, you can also use a decorator
+image_row_selector.add_event_handler(image_row_selector_changed, "selection")
+image_col_selector.add_event_handler(image_col_selector_changed, "selection")
 
 
 figure.show()
-#
-# for subplot in figure:
-#     subplot.camera.zoom = 0.5
+
+for subplot in figure:
+    subplot.camera.zoom = 0.5
 
 
 # NOTE: `if __name__ == "__main__"` is NOT how to use fastplotlib interactively
