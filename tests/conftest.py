8bd77c6d3403c89823419cd06d05e1e82e74f9db
kushalkolar <kushalkolar@gmail.com>
2024-09-27 23:57:08 -0400

    autouse texture limit setup for pytest session

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 MAX_TEXTURE_SIZE = 1024
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def set_wgpu_texture_limit():
     pygfx.renderers.wgpu.set_wgpu_limits(**{"max-texture-dimension2d": MAX_TEXTURE_SIZE})
     yield
