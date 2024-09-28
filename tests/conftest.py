fb883a8f05c6fa839c789201d7e746d3fc9ab186
kushalkolar <kushalkolar@gmail.com>
2024-09-28 00:01:58 -0400

    correct way to set wgpu limits for pytest session

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,8 @@
 import pygfx
 
-import pytest
-
 
 MAX_TEXTURE_SIZE = 1024
 
 
-@pytest.fixture(scope="session", autouse=True)
-def set_wgpu_texture_limit():
+def pytest_sessionstart(session):
     pygfx.renderers.wgpu.set_wgpu_limits(**{"max-texture-dimension2d": MAX_TEXTURE_SIZE})
-    yield
