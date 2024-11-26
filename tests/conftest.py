2d1669ffd3297454a0721e0a3bb777abbdc639fd
Jérémie Fache <jeremie.fache@cea.fr>
2024-11-26 10:45:34 +0100

    fix test config.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,4 @@ MAX_TEXTURE_SIZE = 1024
 
 
 def pytest_sessionstart(session):
-    pygfx.renderers.wgpu.set_wgpu_limits(**{"max-texture-dimension2d": MAX_TEXTURE_SIZE})
+    pygfx.renderers.wgpu.set_wgpu_limits(**{"max-texture-dimension-2d": MAX_TEXTURE_SIZE})
