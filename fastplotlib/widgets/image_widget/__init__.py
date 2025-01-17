9acdb8d913fa1fe51c31e4c6d0cda7601c05c271
kushalkolar <kushalkolar@gmail.com>
2025-01-16 21:17:11 -0500

    raise ModuleNotFoundError if user tries to import imgui and it doesn't exist

--- fastplotlib/widgets/image_widget/__init__.py
+++ fastplotlib/widgets/image_widget/__init__.py
@@ -1,1 +1,13 @@
-from ._widget import ImageWidget
+from ...layouts import IMGUI
+
+if IMGUI:
+    from ._widget import ImageWidget
+
+else:
+
+    class ImageWidget:
+        def __init__(self, *args, **kwargs):
+            raise ModuleNotFoundError(
+                "ImageWidget requires `imgui-bundle` to be installed.\n"
+                "pip install imgui-bundle"
+            )
