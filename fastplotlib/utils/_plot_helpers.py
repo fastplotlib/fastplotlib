beacf81795fddcd18ad2a6ba88d3f7cddddd5dc6
Flynn OConnell <flynnoconnell@gmail.com>
2025-01-18 23:53:16 -0500

    get_nearest_graphics_indices plot helper

--- fastplotlib/utils/_plot_helpers.py
+++ fastplotlib/utils/_plot_helpers.py
@@ -5,14 +5,14 @@ import numpy as np
 from ..graphics._base import Graphic
 from ..graphics._collection_base import GraphicCollection
 
-
-def get_nearest_graphics(
-    pos: tuple[float, float] | tuple[float, float, float],
-    graphics: Sequence[Graphic] | GraphicCollection,
-) -> np.ndarray[Graphic]:
+def get_nearest_graphics_indices(
+        pos: tuple[float, float] | tuple[float, float, float],
+        graphics: Sequence[Graphic] | GraphicCollection,
+) -> np.ndarray[int]:
     """
-    Returns the nearest ``graphics`` to the passed position ``pos`` in world space.
-    Uses the distance between ``pos`` and the center of the bounding sphere for each graphic.
+    Returns indices of the nearest ``graphics`` to the passed position ``pos`` in world space
+    in order of closest to furtherst. Uses the distance between ``pos`` and the center of the
+    bounding sphere for each graphic.
 
     Parameters
     ----------
@@ -25,11 +25,10 @@ def get_nearest_graphics(
 
     Returns
     -------
-    tuple[Graphic]
-        nearest graphics to ``pos`` in order
+    ndarray[int]
+        indices of the nearest nearest graphics to ``pos`` in order
 
     """
-
     if isinstance(graphics, GraphicCollection):
         graphics = graphics.graphics
 
@@ -50,4 +49,30 @@ def get_nearest_graphics(
     distances = np.linalg.norm(centers[:, : len(pos)] - pos, ord=2, axis=1)
 
     sort_indices = np.argsort(distances)
+    return sort_indices
+
+def get_nearest_graphics(
+    pos: tuple[float, float] | tuple[float, float, float],
+    graphics: Sequence[Graphic] | GraphicCollection,
+) -> np.ndarray[Graphic]:
+    """
+    Returns the nearest ``graphics`` to the passed position ``pos`` in world space.
+    Uses the distance between ``pos`` and the center of the bounding sphere for each graphic.
+
+    Parameters
+    ----------
+    pos: (x, y) | (x, y, z)
+        position in world space, z-axis is ignored when calculating L2 norms if ``pos`` is 2D
+
+    graphics: Sequence, i.e. array, list, tuple, etc. of Graphic | GraphicCollection
+        the graphics from which to return a sorted array of graphics in order of closest
+        to furthest graphic
+
+    Returns
+    -------
+    ndarray[Graphic]
+        nearest graphics to ``pos`` in order
+
+    """
+    sort_indices = get_nearest_graphics_indices(pos, graphics)
     return np.asarray(graphics)[sort_indices]
