c26da10d58246f788628d67cc93cc6442f3e7e46
kushalkolar <kushalkolar@gmail.com>
2025-02-27 00:46:05 -0500

    progress

--- fastplotlib/layouts/_subplot_bbox.py
+++ fastplotlib/layouts/_subplot_bbox.py
@@ -82,15 +82,94 @@ class MeshMasks:
 masks = MeshMasks
 
 
-def make_mesh(x1, y1, w, h):
-    geometry = pygfx.plane_geometry(1, 1)
-    material = pygfx.MeshBasicMaterial()
-    plane = pygfx.Mesh(geometry, material)
+class SubplotFrame:
+    def __init__(self, bbox: tuple):
+        x1, y1, w, h = bbox
+        geometry = pygfx.plane_geometry(1, 1)
+        material = pygfx.MeshBasicMaterial()
+        self._plane = pygfx.Mesh(geometry, material)
 
-    plane.geometry.positions.data[masks.x1] = x1
-    plane.geometry.positions.data[masks.x2] = x1 + w
-    plane.geometry.positions.data[masks.y1] = -y1  # negative because UnderlayCamera y is inverted
-    plane.geometry.positions.data[masks.y2] = -(y1 + h)
+        self._resize_handler = pygfx.Points(
+            pygfx.Geometry(positions=[[x1, -y1, 0]]),
+            pygfx.PointsMarkerMaterial(marker="square", size=4, size_space="screen")
+        )
 
-    plane.geometry.positions.update_full()
-    return plane
+        self.rect = bbox
+
+        self._world_object = pygfx.Group()
+        self._world_object.add(self._plane, self._resize_handler)
+
+    def validate_bbox(self, bbox):
+        for val, name in zip(bbox, ["x-position", "y-position", "width", "height"]):
+            if val < 0:
+                raise ValueError(f"Invalid bbox value < 0 for: {name}")
+
+    @property
+    def rect(self) -> tuple[float, float, float, float]:
+        x = self.plane.geometry.positions.data[masks.x1][0]
+        y = self.plane.geometry.positions.data[masks.y1][0]
+
+        w = self.plane.geometry.positions.data[masks.x2][0] - x
+        h = self.plane.geometry.positions.data[masks.y2][0] - y
+
+        return x, -y, w, -h  # remember y is inverted
+
+    @rect.setter
+    def rect(self, bbox: tuple[float, float, float, float]):
+        self.validate_bbox(bbox)
+        x1, y1, w, h = bbox
+
+        x2 = x1 + w
+        y2 = y1 + h
+
+        self._plane.geometry.positions.data[masks.x1] = x1
+        self._plane.geometry.positions.data[masks.x2] = x2
+        self._plane.geometry.positions.data[masks.y1] = -y1  # negative because UnderlayCamera y is inverted
+        self._plane.geometry.positions.data[masks.y2] = -y2
+
+        self._plane.geometry.positions.update_full()
+
+        self._resize_handler.geometry.positions.data[0] = [x2, -y2, 0]
+        self._resize_handler.geometry.positions.update_full()
+
+    @property
+    def plane(self) -> pygfx.Mesh:
+        return self._plane
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
