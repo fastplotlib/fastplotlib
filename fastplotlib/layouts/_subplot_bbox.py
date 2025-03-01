d7f572e8862149984b979d3978ee5c3180f514d9
kushalkolar <kushalkolar@gmail.com>
2025-03-01 04:40:17 -0500

    layout management logic works! :D git status

--- fastplotlib/layouts/_subplot_bbox.py
+++ fastplotlib/layouts/_subplot_bbox.py
@@ -1,5 +1,7 @@
-import pygfx
+from functools import partial
+
 import numpy as np
+import pygfx
 
 
 class UnderlayCamera(pygfx.Camera):
@@ -82,7 +84,7 @@ masks = MeshMasks
 
 
 class SubplotFrame:
-    def __init__(self, figure, rect: np.ndarray = None, ranges: np.ndarray = None):
+    def __init__(self, figure, rect: np.ndarray = None, extent: np.ndarray = None):
         """
 
         Parameters
@@ -92,10 +94,12 @@ class SubplotFrame:
             in absolute screen space or fractional screen space, example if the canvas w, h is (100, 200)
             a fractional rect of (0.1, 0.1, 0.5, 0.5) is (10, 10, 50, 100) in absolute screen space
 
-        ranges (xmin, xmax, ymin, ymax)
-            in absolute screen coordinates or fractional screen coordinates
+        extent: (xmin, xmax, ymin, ymax)
+            range in absolute screen coordinates or fractional screen coordinates
         """
         self.figure = figure
+
+        # canvas (x, y, w, h)
         self._canvas_rect = figure.get_pygfx_render_area()
         figure.canvas.add_event_handler(self._canvas_resized, "resize")
 
@@ -106,25 +110,28 @@ class SubplotFrame:
         self._rect_screen_space = np.zeros(4, dtype=np.float64)
 
         if rect is None:
-            if ranges is None:
+            if extent is None:
                 raise ValueError("Must provide rect or ranges")
 
+            valid, error = self._validate_extent(extent)
+            if not valid:
+                raise ValueError(error)
             # convert ranges to rect
-            rect = self._ranges_to_rect(ranges)
+            rect = self._extent_to_rect(extent)
 
         # assign the internal state of the rect by parsing the user passed rect
         self._assign_rect(rect)
 
         # init mesh of size 1 to graphically represent rect
         geometry = pygfx.plane_geometry(1, 1)
-        material = pygfx.MeshBasicMaterial()
+        material = pygfx.MeshBasicMaterial(pick_write=True)
         self._plane = pygfx.Mesh(geometry, material)
 
         # create resize handler at point (x1, y1)
-        x1, y1 = self.ranges[[1, 3]]
+        x1, y1 = self.extent[[1, 3]]
         self._resize_handler = pygfx.Points(
             pygfx.Geometry(positions=[[x1, -y1, 0]]),  # y is inverted in UnderlayCamera
-            pygfx.PointsMarkerMaterial(marker="square", size=4, size_space="screen")
+            pygfx.PointsMarkerMaterial(marker="square", size=4, size_space="screen", pick_write=True)
         )
 
         self._reset_plane()
@@ -132,18 +139,13 @@ class SubplotFrame:
         self._world_object = pygfx.Group()
         self._world_object.add(self._plane, self._resize_handler)
 
-    def _ranges_to_rect(self, ranges) -> np.ndarray:
-        """convert ranges to rect"""
-        x0, x1, y0, y1 = ranges
+    def _extent_to_rect(self, extent) -> np.ndarray:
+        """convert extent to rect"""
+        x0, x1, y0, y1 = extent
 
         # width and height
         w = x1 - x0
         h = y1 - y0
-
-        if x1 - x0 <= 0:
-            raise ValueError
-        if y1 - y0 <= 0:
-            raise ValueError
 
         x, y, w, h = x0, y0, w, h
 
@@ -178,9 +180,9 @@ class SubplotFrame:
         elif (rect[2:] > 1).all():  # bbox in already in screen coords coordinates
             # check that widths, heights are valid
             if rect[0] + rect[2] > cw:
-                raise ValueError("invalid value: x + width > 1")
+                raise ValueError(f"invalid value: x + width > 1: {rect}")
             if rect[1] + rect[3] > ch:
-                raise ValueError("invalid value: y + height > 1")
+                raise ValueError(f"invalid value: y + height > 1: {rect}")
 
             self._rect_frac[:] = rect / mult
             self._rect_screen_space[:] = rect
@@ -189,15 +191,62 @@ class SubplotFrame:
             raise ValueError(f"Invalid rect: {rect}")
 
     @property
-    def ranges(self) -> np.ndarray:
-        """ranges, (xmin, xmax, ymin, ymax)"""
+    def extent(self) -> np.ndarray:
+        """extent, (xmin, xmax, ymin, ymax)"""
         # not actually stored, computed when needed
         return np.asarray([self.rect[0], self.rect[0] + self.rect[2], self.rect[1], self.rect[1] + self.rect[3]])
 
-    @ranges.setter
-    def ranges(self, ranges: np.ndarray):
-        rect = self._ranges_to_rect(ranges)
+    @extent.setter
+    def extent(self, extent: np.ndarray):
+        valid, error = self._validate_extent(extent)
+
+        if not valid:
+            raise ValueError(error)
+
+        rect = self._extent_to_rect(extent)
         self.rect = rect
+
+    def _validate_extent(self, extent: np.ndarray | tuple) -> tuple[bool, None | str]:
+        x0, x1, y0, y1 = extent
+
+        # width and height
+        w = x1 - x0
+        h = y1 - y0
+
+        # make sure extent is valid
+        if (np.asarray(extent) < 0).any():
+            return False, f"extent ranges must be non-negative, you have passed: {extent}"
+
+        # check if x1 - x0 <= 0
+        if w <= 0:
+            return False, f"extent x-range is invalid: {extent}"
+
+        # check if y1 - y0 <= 0
+        if h <= 0:
+            return False, f"extent y-range is invalid: {extent}"
+
+        # # calc canvas extent
+        # cx0, cy0, cw, ch = self._canvas_rect
+        # cx1 = cx0 + cw
+        # cy1 = cy0 + ch
+        # canvas_extent = np.asarray([cx0, cx1, cy0, cy1])
+
+        # # check that extent is within the bounds of the canvas
+        # if (x0 > canvas_extent[:2]).any() or (x1 > canvas_extent[:2]).any():  # is x0, x1 beyond canvas x-range
+        #     return False, f"extent x-range is beyond the bounds of the canvas: {extent}"
+        #
+        # if (y0 > canvas_extent[2:]).any() or (y1 > canvas_extent[2:]).any():  # is y0, y1 beyond canvas x-range
+        #     return False, f"extent y-range is beyond the bounds of the canvas: {extent}"
+
+        return True, None
+
+    @property
+    def x_range(self) -> np.ndarray:
+        return self.extent[:2]
+
+    @property
+    def y_range(self) -> np.ndarray:
+        return self.extent[2:]
 
     @property
     def rect(self) -> np.ndarray[int]:
@@ -233,6 +282,11 @@ class SubplotFrame:
         """the plane mesh"""
         return self._plane
 
+    @property
+    def resize_handler(self) -> pygfx.Points:
+        """resize handler point"""
+        return self._resize_handler
+
     def _canvas_resized(self, *ev):
         """triggered when canvas is resized"""
         # render area, to account for any edge windows that might be present
@@ -243,6 +297,28 @@ class SubplotFrame:
         # set new rect using existing rect_frac since this remains constant regardless of resize
         self.rect = self._rect_frac
 
+    def is_above(self, y0) -> bool:
+        # our bottom < other top
+        return self.y_range[1] < y0
+
+    def is_below(self, y1) -> bool:
+        # our top > other bottom
+        return self.y_range[0] > y1
+
+    def is_left_of(self, x0) -> bool:
+        # our right_edge < other left_edge
+        # self.x1 < other.x0
+        return self.x_range[1] < x0
+
+    def is_right_of(self, x1) -> bool:
+        # self.x0 > other.x1
+        return self.x_range[0] > x1
+
+    def overlaps(self, extent: np.ndarray) -> bool:
+        """returns whether this subplot would overlap with the other extent"""
+        x0, x1, y0, y1 = extent
+        return not any([self.is_above(y0), self.is_below(y1), self.is_left_of(x0), self.is_right_of(x1)])
+
     def __repr__(self):
         s = f"{self._rect_frac}\n{self.rect}"
 
@@ -250,11 +326,24 @@ class SubplotFrame:
 
 
 class FlexLayoutManager:
-    def __init__(self, figure, *frames: SubplotFrame):
+    def __init__(self, figure, frames: SubplotFrame):
         self.figure = figure
-        self.figure.renderer.add_event_handler(self._figure_resized, "resize")
+        # self.figure.renderer.add_event_handler(self._figure_resized, "resize")
 
         self._frames = frames
+
+        self._last_pointer_pos: np.ndarray[np.float64, np.float64] = np.array([np.nan, np.nan])
+
+        self._moving = False
+        self._resizing = False
+        self._active_frame: SubplotFrame | None = None
+
+        for frame in self._frames:
+            frame.plane.add_event_handler(partial(self._action_start, frame, "move"), "pointer_down")
+            frame.resize_handler.add_event_handler(partial(self._action_start, frame, "resize"), "pointer_down")
+
+        self.figure.renderer.add_event_handler(self._action_iter, "pointer_move")
+        self.figure.renderer.add_event_handler(self._action_end, "pointer_up")
 
     def _subplot_changed(self):
         """
@@ -264,41 +353,90 @@ class FlexLayoutManager:
         """
         pass
 
-    def _figure_resized(self, ev):
-        w, h = ev["width"], ev["height"]
+    # def _figure_resized(self, ev):
+    #     w, h = ev["width"], ev["height"]
 
+    def _new_extent_from_delta(self, delta: tuple[int, int]) -> np.ndarray:
+        delta_x, delta_y = delta
+        if self._resizing:
+            # subtract only from x1, y1
+            new_extent = self._active_frame.extent - np.asarray([0, delta_x, 0, delta_y])
+        else:
+            # moving
+            new_extent = self._active_frame.extent - np.asarray([delta_x, delta_x, delta_y, delta_y])
 
+        x0, x1, y0, y1 = new_extent
+        w = x1 - x0
+        h = y1 - y0
 
+        # make sure width and height are valid
+        if w <= 0:  # width > 0
+            new_extent[:2] = self._active_frame.extent[:2]
 
+        if h <= 0:  # height > 0
+            new_extent[2:] = self._active_frame.extent[2:]
 
+        # ignore movement if this would cause an overlap
+        for frame in self._frames:
+            if frame is self._active_frame:
+                continue
 
+            if frame.overlaps(new_extent):
+                # we have an overlap, need to ignore one or more deltas
+                # ignore x
+                if not frame.is_left_of(x0) or not frame.is_right_of(x1):
+                    new_extent[:2] = self._active_frame.extent[:2]
 
+                # ignore y
+                if not frame.is_above(y0) or not frame.is_below(y1):
+                    new_extent[2:] = self._active_frame.extent[2:]
 
+        # make sure all vals are non-negative
+        if (new_extent[:2] < 0).any():
+            # ignore delta_x
+            new_extent[:2] = self._active_frame.extent[:2]
 
+        if (new_extent[2:] < 0).any():
+            # ignore delta_y
+            new_extent[2:] = self._active_frame.extent[2:]
 
+        # canvas extent
+        cx0, cy0, cw, ch = self._active_frame._canvas_rect
 
+        # check if new x-range is beyond canvas x-max
+        if (new_extent[:2] > cx0 + cw).any():
+            new_extent[:2] = self._active_frame.extent[:2]
 
+        # check if new y-range is beyond canvas y-max
+        if (new_extent[2:] > cy0 + ch).any():
+            new_extent[2:] = self._active_frame.extent[2:]
 
+        return new_extent
 
+    def _action_start(self, frame: SubplotFrame, action: str, ev):
+        if ev.button == 1:
+            if action == "move":
+                self._moving = True
+            elif action == "resize":
+                self._resizing = True
+                frame.resize_handler.material.color = (1, 0, 0)
+            else:
+                raise ValueError
 
+            self._active_frame = frame
+            self._last_pointer_pos[:] = ev.x, ev.y
 
+    def _action_iter(self, ev):
+        if not any((self._moving, self._resizing)):
+            return
 
+        delta_x, delta_y = self._last_pointer_pos - (ev.x, ev.y)
+        new_extent = self._new_extent_from_delta((delta_x, delta_y))
+        self._active_frame.extent = new_extent
+        self._last_pointer_pos[:] = ev.x, ev.y
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def _action_end(self, ev):
+        self._moving = False
+        self._resizing = False
+        self._active_frame.resize_handler.material.color = (0, 0, 0)
+        self._last_pointer_pos[:] = np.nan
