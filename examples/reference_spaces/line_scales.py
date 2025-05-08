9bfd8164af9cc82b97385bbc524d2ad20b530ff3
kushalkolar <kushalkolar@gmail.com>
2025-05-07 21:47:57 -0400

    example

--- examples/reference_spaces/line_scales.py
+++ examples/reference_spaces/line_scales.py
@@ -1,6 +1,8 @@
 import numpy as np
 import fastplotlib as fpl
-
+from fastplotlib.ui import DebugWindow
+import pygfx
+from icecream import ic
 
 xs = np.linspace(0, 10 * np.pi, 1000)
 ys = np.sin(xs)
@@ -16,9 +18,42 @@ fig[0, 0].add_line(l1)
 fig.show(maintain_aspect=False)
 fig[0, 0].auto_scale(zoom=0.4)
 
-rs = fig[0, 0].add_reference_space(scale=(1, 500, 1))
+rs = fig[0, 0].add_reference_frame(
+    scale=(1, 500, 1),
+)
 l2 = fig[0, 0].add_line(l2, reference_space=rs, colors="r")
 l2.add_axes(rs)
-l2.axes.y.line.material.color = "r"
+l2.axes.y.line.material.color = "m"
+
+
+@fig.renderer.add_event_handler("key_down")
+def change_y_scale(ev: pygfx.KeyboardEvent):
+    if ev.key != "1":
+        return
+
+    rs.controller.remove_camera(rs.camera)
+    rs.controller.add_camera(rs.camera, include_state={"height"})
+
+    fig[0, 0].controller.enabled = False
+
+
+@fig.renderer.add_event_handler("key_down")
+def change_y_scale(ev: pygfx.KeyboardEvent):
+    if ev.key != "0":
+        return
+
+    rs.controller.remove_camera(rs.camera)
+    rs.controller.add_camera(rs.camera)
+    fig[0, 0].controller.enabled = True
+
+
+debug_objs = [
+    fig[0, 0].camera.get_state,
+    rs.camera.get_state
+]
+
+debug_window = DebugWindow(debug_objs)
+fig.add_gui(debug_window)
+
 
 fpl.loop.run()
