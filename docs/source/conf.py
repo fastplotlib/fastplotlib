d0f003cb253b73fd88427a1165bd1ae82efc6688
kushalkolar <kushalkolar@gmail.com>
2025-05-30 00:59:06 -0400

    see if this works to remove jupyter nb download button in gallery

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ extensions = [
 
 sphinx_gallery_conf = {
     "gallery_dirs": "_gallery",
+    "notebook_extensions": {},
     "backreferences_dir": "_gallery/backreferences",
     "doc_module": ("fastplotlib",),
     "image_scrapers": ("pygfx",),
