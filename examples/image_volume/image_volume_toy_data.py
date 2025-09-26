76a92f3908483470342a991bfa768aeeb3ddac63
kushalkolar <kushalkolar@gmail.com>
2025-09-26 02:33:01 -0400

    add example

---
+++ examples/image_volume/image_volume_toy_data.py
@@ -0,0 +1,31 @@
+"""
+Volume rendering of toy data
+============================
+
+Volume rendering of toy trig data
+"""
+
+import fastplotlib as fpl
+import numpy as np
+
+n_cols = 100
+n_rows = 100
+z = 50
+
+xs = np.linspace(0, 1_000, n_cols)
+
+sine = np.sin(np.sqrt(xs))
+
+data = np.dstack([np.vstack([sine * i for i in range(n_rows)]).astype(np.float32) * j for j in range(z)])
+
+fig = fpl.Figure(cameras="3d", controller_types="orbit")
+
+volume = fig[0, 0].add_image_volume(data)
+
+fig.show()
+
+# NOTE: fpl.loop.run() should not be used for interactive sessions
+# See the "JupyterLab and IPython" section in the user guide
+if __name__ == "__main__":
+    print(__doc__)
+    fpl.loop.run()
