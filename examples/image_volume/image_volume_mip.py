7bac1115888127826b963c324f13b65ffec998b2
kushalkolar <kushalkolar@gmail.com>
2025-09-28 05:15:28 -0400

    upate example

--- examples/image_volume/image_volume_mip.py
+++ examples/image_volume/image_volume_mip.py
@@ -16,9 +16,28 @@ voldata = iio.imread("imageio:stent.npz").astype(np.float32)
 
 fig = fpl.Figure(cameras="3d", controller_types="orbit", size=(700, 560))
 
-fig[0, 0].add_image_volume(voldata, mode="mip")
+fig[0, 0].add_image_volume(voldata, mode="mip", alpha_mode="add")
 
 fig.show()
+
+
+# load a pre-saved camera state
+state = {
+    "position": np.array([-120, 90, 330]),
+    "rotation": np.array([-0.07280538, -0.41100206, -0.03295049, 0.90812496]),
+    "scale": np.array([1.0, 1.0, 1.0]),
+    "reference_up": np.array([0.0, 1.0, 0.0]),
+    "fov": 50.0,
+    "width": 128.0,
+    "height": 128.0,
+    "depth": 313,
+    "zoom": 0.75,
+    "maintain_aspect": True,
+    "depth_range": None,
+}
+
+
+fig[0, 0].camera.set_state(state)
 
 
 # NOTE: fpl.loop.run() should not be used for interactive sessions
