66ab13088bb409beb14f3a7578e9496f0ee2571c
kushalkolar <kushalkolar@gmail.com>
2025-11-08 04:30:17 -0500

    add ArrayProtocol

---
+++ fastplotlib/utils/_protocols.py
@@ -0,0 +1,18 @@
+from typing import Protocol, runtime_checkable
+
+
+ARRAY_LIKE_ATTRS = ["shape", "ndim", "__getitem__"]
+
+
+@runtime_checkable
+class ArrayProtocol(Protocol):
+    @property
+    def ndim(self) -> int:
+        ...
+
+    @property
+    def shape(self) -> tuple[int, ...]:
+        ...
+
+    def __getitem__(self, key):
+        ...
