d944f5549fb99dbd434ff84c88fab9744b7ef32d
Almar Klein <almar@almarklein.org>
2025-11-28 10:47:18 +0100

    restore line example

--- examples/line/line.py
+++ examples/line/line.py
@@ -10,19 +10,31 @@ Example showing cosine, sine, sinc lines.
 
 import fastplotlib as fpl
 import numpy as np
-import pygfx as gfx
 
+figure = fpl.Figure(size=(700, 560))
 
-figure = fpl.Figure(size=(700, 560), cameras='3d', controller_types='orbit')
+xs = np.linspace(-10, 10, 100)
+# sine wave
+ys = np.sin(xs)
+sine_data = np.column_stack([xs, ys])
 
+# cosine wave
+ys = np.cos(xs) + 5
+cosine_data = np.column_stack([xs, ys])
 
+# sinc function
+a = 0.5
+ys = np.sinc(xs) * 3 + 8
+sinc_data = np.column_stack([xs, ys])
 
-geo = gfx.geometries.torus_knot_geometry()
-positions = geo.positions.data
-indices = geo.indices.data
+sine = figure[0, 0].add_line(data=sine_data, thickness=5, colors="magenta")
 
-mesh = figure[0, 0].add_mesh(positions, indices, colors="magenta")
+# you can also use colormaps for lines!
+cosine = figure[0, 0].add_line(data=cosine_data, thickness=12, cmap="autumn")
 
+# or a list of colors for each datapoint
+colors = ["r"] * 25 + ["purple"] * 25 + ["y"] * 25 + ["b"] * 25
+sinc = figure[0, 0].add_line(data=sinc_data, thickness=5, colors=colors)
 
 figure[0, 0].axes.grids.xy.visible = True
 figure.show()
