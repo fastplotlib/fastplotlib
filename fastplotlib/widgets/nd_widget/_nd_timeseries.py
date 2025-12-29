d5e4c7d45901b1f5f2de89e68ef4d416d0ea7dde
kushalkolar <kushalkolar@gmail.com>
2025-12-29 01:44:11 -0800

    NDPositions, basics work, reorganize, increase default scatter size

--- fastplotlib/widgets/nd_widget/_nd_timeseries.py
+++ fastplotlib/widgets/nd_widget/_nd_timeseries.py
@@ -5,84 +5,10 @@ from warnings import warn
 import numpy as np
 from numpy.typing import ArrayLike
 
-from ...graphics import ImageGraphic, LineStack, LineCollection, ScatterGraphic
 from ...utils import subsample_array, ArrayProtocol
 
-
-# must take arguments: array-like, `axis`: int, `keepdims`: bool
-WindowFuncCallable = Callable[[ArrayLike, int, bool], ArrayLike]
-
-
-class NDProcessor:
-    def __init__(
-        self,
-        data,
-        n_display_dims: Literal[2, 3] = 2,
-        slider_index_maps: tuple[Callable[[Any], int] | None, ...] | None = None,
-        window_funcs: tuple[WindowFuncCallable | None] | None = None,
-        window_sizes: tuple[int | None] | None = None,
-        spatial_func: Callable[[ArrayProtocol], ArrayProtocol] | None = None,
-    ):
-        self._data = self._validate_data(data)
-        self._slider_index_maps = self._validate_slider_index_maps(slider_index_maps)
-
-    @property
-    def data(self) -> ArrayProtocol:
-        return self._data
-
-    @data.setter
-    def data(self, data: ArrayProtocol):
-        self._data = self._validate_data(data)
-
-    def _validate_data(self, data: ArrayProtocol):
-        if not isinstance(data, ArrayProtocol):
-            raise TypeError("`data` must implement the ArrayProtocol")
-
-        return data
-
-    @property
-    def window_funcs(self) -> tuple[WindowFuncCallable | None] | None:
-        pass
-
-    @property
-    def window_sizes(self) -> tuple[int | None] | None:
-        pass
-
-    @property
-    def spatial_func(self) -> Callable[[ArrayProtocol], ArrayProtocol] | None:
-        pass
-
-    @property
-    def slider_dims(self) -> tuple[int, ...] | None:
-        pass
-
-    @property
-    def slider_index_maps(self) -> tuple[Callable[[Any], int] | None, ...]:
-        return self._slider_index_maps
-
-    @slider_index_maps.setter
-    def slider_index_maps(self, maps):
-        self._maps = self._validate_slider_index_maps(maps)
-
-    def _validate_slider_index_maps(self, maps):
-        if maps is not None:
-            if not all([callable(m) or m is None for m in maps]):
-                raise TypeError
-
-        return maps
-
-    def __getitem__(self, item: tuple[Any, ...]) -> ArrayProtocol:
-        pass
-
-
-class NDImageProcessor(NDProcessor):
-    @property
-    def n_display_dims(self) -> Literal[2, 3]:
-        pass
-
-    def _validate_n_display_dims(self, n_display_dims):
-        if n_display_dims not in (2, 3):
-            raise ValueError("`n_display_dims` must be")
+from ...graphics import ImageGraphic, LineStack, LineCollection, ScatterGraphic
+from ._processor_base import NDProcessor, WindowFuncCallable
 
 
 VALID_TIMESERIES_Y_DATA_SHAPES = (
@@ -144,32 +70,6 @@ class NDTimeSeriesProcessor(NDProcessor):
             )
 
         return data
-
-    @property
-    def display_graphic(self) -> Literal["line", "heatmap"]:
-        return self._display_graphic
-
-    @display_graphic.setter
-    def display_graphic(self, dg: Literal["line", "heatmap"]):
-        dg = self._validate_display_graphic(dg)
-
-        if dg == "heatmap":
-            # check if x-vals uniformly spaced
-            # this is very fast to do on the fly, especially for typical small display windows
-            norm = np.linalg.norm(np.diff(np.diff(self.x_values))) / len(self.x_values)
-            if norm > 10 ** -12:
-                # need to create evenly spaced x-values
-                x0 = self.data[0][0]
-                xn = self.data[0][-1]
-                self._uniform_x_values = np.linspace(x0, xn, num=len(self.data[0]))
-
-                # TODO: interpolate yz values on the fly only when within the display window
-
-    def _validate_display_graphic(self, dg):
-        if dg not in ("line", "heatmap"):
-            raise ValueError
-
-        return dg
 
     @property
     def display_window(self) -> int | float | None:
