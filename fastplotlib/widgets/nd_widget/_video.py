1ec8e4b095666fd491b9ab64c582ec449e4e546f
kushalkolar <kushalkolar@gmail.com>
2026-08-22 12:06:56 -0400

    bugfix after last PR

--- fastplotlib/widgets/nd_widget/_video.py
+++ fastplotlib/widgets/nd_widget/_video.py
@@ -1,11 +1,14 @@
-from ._nd_image import NDImageProcessor, NDImage
 from typing import Callable, Any, Literal
 
 import numpy as np
 
+from ...graphics.image import TupleYUV
+from ._nd_image import NDImageProcessor
+from ._async import run_in_thread_pool
+
 
 class VideoProcessor(NDImageProcessor):
-    async def get_window_output(self, indices: dict[str, Any]):
+    async def get_window_output(self, indices: dict[str, Any]) -> TupleYUV | np.ndarray:
         """
         Applies any window functions and returns squeezed sliced array transposed in the order of the given spatial dims
 
@@ -25,3 +28,20 @@ class VideoProcessor(NDImageProcessor):
 
         # convert to numpy array
         return np.asarray(windowed_slice).squeeze()
+
+    async def get(self, indices: dict[str, Any]) -> TupleYUV | np.ndarray:
+        """
+        Similar to NDImage.get() but accounts for TupleYUV output.
+        """
+        # this will be squeezed output, with dims in the order of the user set spatial dims
+        window_output = await self.get_window_output(indices)
+
+        if self.spatial_func is not None:
+            window_output = await run_in_thread_pool(
+                self._executor, self._spatial_func, window_output
+            )
+
+        if isinstance(window_output, tuple):
+            return tuple(a.transpose(*self.spatial_dims_indices) for a in window_output)
+
+        return window_output.transpose(*self.spatial_dims_indices)
