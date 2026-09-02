0284cfa0aabe049aa3a85347f5f3bfa7f7da3de1
kushalkolar <kushalkolar@gmail.com>
2026-09-01 20:32:10 -0400

    image collection examples

---
+++ examples/image_collection/image_collection.py
@@ -0,0 +1,34 @@
+"""
+Image Collection
+================
+
+Position a collection of images individually using per-image ``offsets``.
+"""
+
+# test_example = false
+# sphinx_gallery_pygfx_docs = 'screenshot'
+
+import numpy as np
+import fastplotlib as fpl
+
+rng = np.random.default_rng(0)
+
+# a collection of random 100 x 100 images
+images = [rng.random((100, 100), dtype=np.float32) for _ in range(5)]
+
+figure = fpl.Figure(size=(700, 560))
+
+# stagger the images diagonally with one (x, y, z) offset per image
+offsets = np.array([[i * 60, -i * 60, 0] for i in range(len(images))])
+
+figure[0, 0].add_image_collection(images, offsets=offsets, cmap="viridis")
+
+figure[0, 0].axes.visible = False
+
+figure.show()
+
+# NOTE: fpl.loop.run() should not be used for interactive sessions
+# See the "JupyterLab and IPython" section in the user guide
+if __name__ == "__main__":
+    print(__doc__)
+    fpl.loop.run()
