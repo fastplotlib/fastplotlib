efd31044532e378ce5c222e4545750cfa1dfabe7
kushalkolar <kushalkolar@gmail.com>
2026-09-15 22:27:39 -0400

    fix import

--- fastplotlib/widgets/__init__.py
+++ fastplotlib/widgets/__init__.py
@@ -1,4 +1,5 @@
 from .nd_widget import *
+from .nd_widget import __all__ as _nd_widget_all
 from .image_widget import ImageWidget
 
-__all__ = ["NDWidget", "ImageWidget"]
+__all__ = [*_nd_widget_all, "ImageWidget"]
