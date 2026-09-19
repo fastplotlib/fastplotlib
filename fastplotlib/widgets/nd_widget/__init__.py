85de8d1ff3c3b544a310406033476848e88991e7
kushalkolar <kushalkolar@gmail.com>
2026-09-19 04:25:34 -0400

    nds extras

--- fastplotlib/widgets/nd_widget/__init__.py
+++ fastplotlib/widgets/nd_widget/__init__.py
@@ -3,7 +3,7 @@ from ...layouts import IMGUI
 if IMGUI:
     from ._index import RangeContinuous, AutoRangeContinuous, ReferenceIndices
     from ._base import NDSlicer, NDGraphic
-    from ._nd_positions import NDPositions, NDPositionsSlicer, NDTimeseries, nds_extras
+    from ._nd_positions import NDPositions, NDPositionsSlicer, NDTimeseries
     from ._nd_image import NDImageSlicer, NDImage
     from ._video import VideoSlicer
     from ._nd_vectors import NDVectorsSlicer, NDVectors
@@ -19,7 +19,6 @@ if IMGUI:
         "NDPositions",
         "NDPositionsSlicer",
         "NDTimeseries",
-        "nds_extras",
         "NDImageSlicer",
         "NDImage",
         "VideoSlicer",
