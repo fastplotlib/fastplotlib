7b3ec6b7c5512d4600e7bb3e1ed023d04ef81fcf
Kushal Kolar <kushalkolar@gmail.com>
2026-09-23 19:45:08 -0400

    Update _version.py

--- fastplotlib/_version.py
+++ fastplotlib/_version.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 # This is the reference version number, to be bumped before each release.
 # The build system detects this definition when building a distribution.
-__version__ = "0.6.1"
+__version__ = "0.7.0"
 
 # Allow using nearly the same code in different projects
 project_name = "fastplotlib"
